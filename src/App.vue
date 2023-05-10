eea143394a06625e8788b0df527ede5a29aa2917
asma <as.ch.2280@gmail.com>
2023-05-10 23:46:36 +0330

    add TableProduct component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <TableProducts/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import TableProducts from './components/TableProducts.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TableProducts
   }
 }
 </script>
---
+++ src/components/TableProducts.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="header">
+    <p v-for="headerName in headerNames" :key="headerName" class="header-name">
+      {{ headerName }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TableProducts",
+  data() {
+    return {
+      headerNames: [
+        " ",
+        "ردیف",
+        "کد کالا",
+        "َشرح کالا یا خدمات",
+        "تعداد / مقدار",
+        "واحد اندازه گیری",
+        "مبلغ واحد (ریال)",
+        "مبلغ کل (ریال)",
+        "مبلغ تخفیف",
+        "مبلغ کل پس از تخفیف(ریال)",
+        "جمع مالیات و عوارض (ریال)",
+        "جمع مبلغ کل بعلاوه جمع مالیات و عوارض (ریال)",
+      ],
+      tableDat: [
+        {
+          selected: false,
+          rownNumber: 1,
+          ProductCode: 125,
+          DescriptionOfGoods: "کالای خوبی است",
+          number: 14,
+          unit: "kg",
+          UnitAmount: "120000",
+          TotalAmount: "6564686979876",
+          amountDiscount: "768798",
+          TotalAmountAfterDiscount: "87987987",
+          taxCollection: 546879879,
+          sumOfTotalAmountPlusTax: 467879879879798,
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+.header {
+  display: flex;
+  flex-direction: row-reverse;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+}
+.header-name {
+  border: 1px solid #000;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 36px;
+}
+</style>
